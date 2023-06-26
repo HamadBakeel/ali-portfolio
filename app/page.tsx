79f059cd13a3ce45ea6e0f63e138174b9e61f519
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-06-26 22:17:59 +0300

    feat: home page responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,37 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 w-full md:px-28  min-h-screen items-center">
+    <main className="grid grid-cols-1 md:grid-cols-2 w-full min-h-screen items-center justify-items-center px-[18px] py-[80px] md:px-[4vw] lg:px-[6vw] xl:px-[10vw]">
       <section className="relative isolate">
-        <h1 className=" text-[60px] uppercase leading-[55px] mb-4">
+        <h1 className=" text-[50px] md:text-[60px] uppercase leading-[55px] mb-4">
           My name <br /> is{" "}
           <span className=" font-extrabold">
-            Ali <br /> Abdu...
+            Ali <br className=" hidden md:block"/> Abdu...
           </span>
         </h1>
-        <h2 className=" italic font-bold">
+        <h2 className=" italic font-bold text-xl md:text-[initial]">
           Product Designer <span className=" font-normal">based in</span> Riyadh
         </h2>
 
+        <Image
+          className=" min-w-[40vw] md:hidden"
+          src="/assets/ali/home-image.svg"
+          alt=""
+          width={450}
+          height={450}
+        />
+
         <button
           className="
-          bg-sport-black hover:border
-          hover:bg-white hover:text-black transition
-          text-white px-6 py-2 mt-7
-           text-sm
-          flex justify-center items-center gap-2
+          bg-sport-black border
+          hover:bg-white hover:text-black transition text-white
+           md:text-sm flex justify-center items-center gap-2
+           mx-auto md:mx-0 mb-6 px-4 py-4 md:px-6 md:py-2 mt-2 md:mt-7
           "
         >
           Let’s talk with me
           <Image
+          className=" w-[25px] md:w-[20px]"
             src="/assets/icons/colored-arrow.svg"
             alt=""
             width={15}
@@ -32,10 +40,10 @@ export default function Home() {
           />
         </button>
 
-        <div className=" md:mt-10 flex gap-8">
+        <div className=" md:mt-10 flex gap-1 md:gap-8 flex-col md:flex-row items-center">
           <span className=" flex gap-2 font-bold italic">
             <Image
-              className=" bg-[#1413130D] rounded-full px-1"
+              className=" bg-[#1413130D] rounded-full px-1 max-h-[25px]"
               src="/assets/icons/phone.svg"
               alt=""
               width={25}
@@ -45,7 +53,7 @@ export default function Home() {
           </span>
           <span className=" flex gap-2 font-bold italic">
             <Image
-              className=" bg-[#1413130D] rounded-full px-1"
+              className=" bg-[#1413130D] rounded-full px-1 max-h-[25px]"
               src="/assets/icons/envelope.svg"
               alt=""
               width={25}
@@ -56,7 +64,7 @@ export default function Home() {
         </div>
 
         <Image
-          className=" absolute -top-1 -left-6 -z-[1]"
+          className=" absolute -top-1 -left-6 -z-[1] invisible md:visible"
           src="/assets/shapes/home-name-top-cut.svg"
           alt=""
           width={120}
@@ -64,9 +72,9 @@ export default function Home() {
         />
       </section>
 
-      <section className="relative isolate">
+      <section>
         <Image
-          className=" min-w-[40vw]"
+          className=" min-w-[40vw] hidden md:block"
           src="/assets/ali/home-image.svg"
           alt=""
           width={450}
