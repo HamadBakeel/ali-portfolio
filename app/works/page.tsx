95bf47ae51d4b16526ceddfb03be56bbb0f2913e
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-06-29 04:44:06 +0300

    feat: work page title and subtitle

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,8 +1,13 @@
+import PageSubtitle from '@/components/pageSubtitle'
+import PageTitle from '@/components/pageTitle'
 import React from 'react'
 
 function Works() {
   return (
-    <div>Works</div>
+    <main className="w-full min-h-screen pt-24 px-8 md:px-[3vw] lg:px-[5vw] xl:px-[7vw] max-w-screen-xl">
+      <PageTitle text='Work'/>
+      <PageSubtitle text="recent projects"/>
+    </main>
   )
 }
 
